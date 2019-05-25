8a59b3b777ade1306eefb07bc682c918018f0fbe
Artem Cherepnin <artnerab@gmail.com>
2019-05-25 20:48:59 +0300

    sizes, tabs

--- picture_art/js/main.js
+++ picture_art/js/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const done = () => console.log('done');
+	const log = (msg) => console.log(msg);
 	// Слайдер
 	let slideIndex = 1,
 		slides = document.querySelectorAll('.main-slider-item');
@@ -59,9 +60,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Больше стилей
 	let styles = document.querySelectorAll('.styles-2'),
 		styleBtn = document.querySelector('.button-styles');
+		
 
 	styleBtn.addEventListener('click', () => {
-		styleBtn.display = 'none';
+		styleBtn.style.display = 'none';
 		styles.forEach((e) => {
 			e.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
 			e.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
@@ -98,57 +100,61 @@ window.addEventListener('DOMContentLoaded', () => {
 		if (target.tagName == 'SELECT' || target.tagName == 'INPUT') priceCalc();
 	});
 
-	// size.addEventListener('change', function() {
-	// 	priceCalc();
-	// });
+	// Табы 
 
-	// material.addEventListener('change', function() {
-	// 	priceCalc();
-	// });
-
-	// options.addEventListener('change', function() {
-	// 	priceCalc();
-	// });
-
-	// promocode.addEventListener('change', function() {
-	// 	priceCalc();
-	// });
-
-	// size.addEventListener('change', function() {
-	// 	this.options[0].disabled = true;
-	// 	sizeSum = +this.value;
-	// 	if (options.options.selectedIndex > 0 && materialSum != 0) {
-	// 		total = sizeSum * materialSum + optionsSum;
-	// 		price.innerHTML = total;
-	// 	} else if (materialSum != 0) {
-	// 		total = sizeSum * materialSum;
-	// 		price.innerHTML = total;
-	// 	}
-	// });
-
-	// material.addEventListener('change', function() {
-	// 	this.options[0].disabled = true;
-	// 	materialSum = +this.value;
-	// 	if (options.options.selectedIndex > 0 && size != 0) {
-	// 		total = sizeSum * materialSum + optionsSum;
-	// 		price.innerHTML = total;
-	// 	} else if (sizeSum != 0) {
-	// 		total = sizeSum * materialSum;
-	// 		price.innerHTML = total;
-	// 	}
-	// });
-
-	// options.addEventListener('change', function() {
-	// 	optionsSum = +this.value;
-	// 	if(options.options.selectedIndex > 0 && sizeSum != 0 && materialSum != 0) {
-	// 		total = sizeSum * materialSum + optionsSum;
-	// 		price.innerHTML = total;
-	// 	} else {
-	// 		price.innerHTML = total;
-	// 	}
-	// });
+	let tabBlock = document.querySelector('.portfolio-menu'),
+		tabs = document.querySelectorAll('.portfolio-menu > li'),
+		tabContent = document.querySelectorAll('.portfolio-block'),
+		noContent = document.querySelector('.portfolio-no');
 
 
-	
-	
+
+	tabBlock.addEventListener('click', event => {
+		tabs.forEach((item) => {
+			item.classList.remove('active');
+		});
+		let target = event.target,
+			targetClass = target.getAttribute('class'),
+			empty = true;
+		target.classList.add('active');
+		if (target && target.tagName == 'LI') {
+			for (let i = 0; i < tabContent.length; i++) {
+				if (tabContent[i].classList.contains(targetClass)) {
+					tabContent[i].style.display  = 'block';
+					empty = false;
+				} else tabContent[i].style.display = 'none';
+
+			}
+			if (empty) noContent.style.display = 'block';
+		}
+	});
+
+	// Размеры
+
+	let sizesWrap = document.querySelector('.sizes-wrapper');
+
+	const bindPicture = (target, picDisplay) => {
+		let targetParent = target.parentElement.querySelectorAll('p');
+		targetParent.forEach((item) => {
+			item.style.display = picDisplay;
+		});
+	};
+					
+	sizesWrap.addEventListener('mouseover', (e) => {
+		let target = e.target;
+		if (target && target.tagName == 'IMG') {
+			bindPicture(target, 'none');
+			let targetClass = target.getAttribute('class');
+			target.setAttribute('src', `img/${targetClass}-1.png`);
+		}
+	});
+
+	sizesWrap.addEventListener('mouseout', (e) => {
+		let target = e.target;
+		if (target && target.tagName == 'IMG') {
+			bindPicture(target, 'block');
+			let targetClass = target.getAttribute('class');
+			target.setAttribute('src', `img/${targetClass}.png`);
+		}
+	});
 });
